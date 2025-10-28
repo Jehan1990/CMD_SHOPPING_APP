522d03377cd5d244caced7f23f892e573f4d41f1
jehan ichhaporia <jehan_ichhaporia@hotmail.com>
2025-10-28 15:16:41 +0530

    add

--- checkout.dart
+++ checkout.dart
@@ -8,13 +8,11 @@ class Product {
   //constructor
   const Product({required this.id, required this.name, required this.price});
   //getter initial is a method to get the first charecter of the product name
-  String get initial=>name.substring(0,1);
-  //getter display name is a method to get name and price of the product 
-  //by concatinating string using initial getter. 
-  String get displayName=>'($initial)${name.substring(1)} :\$$price';
-
+  String get initial => name.substring(0, 1);
+  //getter display name is a method to get name and price of the product
+  //by concatinating string using initial getter.
+  String get displayName => '($initial)${name.substring(1)} :\$$price';
 }
-
 
 class Item {
   //constructor
@@ -24,10 +22,35 @@ class Item {
   final int quantity;
   //getter
   double get price => quantity * product.price;
+
+  @override
+  String toString() => '$quantity x ${product.name} \$$price';
 }
 
 class Cart {
-  //TODO:Item collection
+  final Map<int, Item> _item = {};
+
+  void addProduct(Product product) {
+    final item = _item[product.id];
+    if (item == null) {
+      _item[product.id] = Item(product: product, quantity: 1);
+    } else {
+      _item[product.id] = Item(product: product, quantity: item.quantity + 1);
+    }
+  }
+
+  double total() => _item.values
+      .map((item) => item.price)
+      .reduce((value, element) => value + element);
+
+  @override
+  String toString() {
+    if (_item.isEmpty) {
+      return 'Cart is empty';
+    }
+    final itemisedList = _item.values.map((item) => item.toString()).join('\n');
+    return '$itemisedList\nTotal: \$${total()}\n------------';
+  }
 }
 
 //allProduct list with Product objects
@@ -56,38 +79,40 @@ end
 */
 
 void main() {
+  final cart = Cart();
   while (true) {
-    //writes the first line of question to ask user what they want to do. 
+    //writes the first line of question to ask user what they want to do.
     //It uses dart libray dart:io for stdout=standard_output
-    stdout.write(
-      'What do you want to do? (v)iew item, (a)dd item, (c)heckout ',
-    );
-    //reads user input using stdin.readLineSync() from dart:io libray 
+    stdout.write('''(v)iew item,\n(a)dd item,\n(c)heckout,\n(q)uit
+What do you want to do? ''');
+    //reads user input using stdin.readLineSync() from dart:io libray
     final line = stdin.readLineSync();
     //perform conditional logic using user input and chooseProduct() function.
     if (line == 'a') {
-      final product=chooseProduct();
-      if(product !=null){
-        print('\n${product.displayName}');
+      final product = chooseProduct();
+      if (product != null) {
+        cart.addProduct(product);
+        print('\n----CART----\n$cart');
       }
     } else if (line == 'v') {
-      //TODO:implement
+      print('\n----CART----\n$cart');
     } else if (line == 'c') {
       //TODO:implement
+    } else if (line == 'q') {
+      break;
     } else {
-      print('Wrong input selected');
+      print('Wrong Input selected');
     }
   }
 }
-
 
 //Function chooseProduct() which returns Product which is nullable
 Product? chooseProduct() {
   final productList = allProducts
       .map((product) => product.displayName)
-      .join('\n');//productList is of type String(multiline)
+      .join('\n'); //productList is of type String(multiline)
 
-//Display the product list for the user to choose the items in the cart
+  //Display the product list for the user to choose the items in the cart
   stdout.write('\nAvailable Products:\n$productList\n\nYour Choice: ');
   //reads the user input
   final line = stdin.readLineSync();
